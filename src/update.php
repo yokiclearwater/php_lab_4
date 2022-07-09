1c8b99d0b4f500566b92cbed7867c339bf525be7
kimhonggithub <kimhong.set@student.cadt.edu.kh>
2022-07-09 23:22:38 +0700

    update

--- src/update.php
+++ src/update.php
@@ -42,7 +42,6 @@ if (isset($_POST['submit'])) {
     <title>Add Product</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-
 <body>
     <div class="container mt-5 pt-5">
         <div class="col-8 m-auto">
